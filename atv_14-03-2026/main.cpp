ce48366e4e495331b1efd49a673f05642e6bc98c
jaoalencar <ra197286@ucdb.br>
2026-03-25 19:26:23 -0400

    Finalizada atividade

--- atv_14-03-2026/main.cpp
+++ atv_14-03-2026/main.cpp
@@ -62,6 +62,24 @@ typedef struct
         //se retornar -1 é pq não encontrou
         return pos;
     }
+    bool remove(REGISTRO value){
+        int i = busca(value); //Item encontrado
+        //Valida condições antes da remoção
+        if (i<0){
+            cout << "Item nao pertence a lista" << endl;
+            return false;
+        }else if(n_registros < 1){
+            cout << "Lista vazia" << endl;
+        }
+
+        //Remove o item preenchendo com os valores seguintes
+        for (size_t j = i; j < n_registros-1; j++)
+        {
+            A[j] = A[j+1]; //Arrasta a lista
+        }
+        n_registros--;
+        cout << "Lista atualizada, n_registros: " << n_registros << " \n";
+    }
 }LISTA;
 
 int main() {
@@ -71,7 +89,8 @@ int main() {
     int encontrado; //posição do item que foi encontrado
     bool resp; //resposta da inserção
 
-    //INSERINDO ITENS NA LISTA
+
+    //----------------INSERINDO ITENS NA LISTA---------------
     cout << "-----1 ETAPA - INSERCAO DE VALORES-------"<<endl;
     aux.chave = 1;
     resp = l.inserir(aux, 0); //insere na posição correta
@@ -101,6 +120,54 @@ int main() {
     }else{
         cout << "FALHA AO INSERIR!";
     }
+    cout << endl << endl;
 
+    //--------------------POVOANDO O RESTANTE DA LISTA ---------------------
+    for (size_t i = 0; i < 10; i++)
+    {
+        aux.chave = i*3;
+        l.inserir(aux, l.n_registros);
+    }
+    //--------------BUSCANDO UM ITEM NA LISTA--------------
+    //Com um valor que não existe
+    aux.chave = 7; //esse será o valor buscado na lista
+    cout << "-----2 ETAPA - BUSCA DE VALORES-------"<<endl;
+    cout << "Buscando valor " << aux.chave << " na lista l" << endl;
+    encontrado = l.busca(aux);
+    if (encontrado >= 0) cout << "Valor encontrado na posição " << encontrado << " da lista";
+    else cout << "Valor nao encontrado";
+    cout << endl << endl;
+
+    //Com um valor que existe
+    aux.chave = 15; //esse será o valor buscado na lista
+    cout << "Buscando valor " << aux.chave << " na lista l" << endl;
+    encontrado = l.busca(aux);
+    if (encontrado >= 0) cout << "Valor encontrado na posicao " << encontrado << " da lista (" << l.A[encontrado].chave << ")";
+    else cout << "Valor nao encontrado";
+    cout << endl << endl;
+
+    //--------------REMOVENDO UM ITEM NA LISTA--------------
+    //Com um valor que não existe
+    aux.chave = 7; //esse será o valor buscado na lista
+    cout << "-----3 ETAPA - REMOCAO DE VALORES-------"<<endl;
+    resp = l.remove(aux);
+    if (resp){
+        cout << "Item removido da lista!!";
+    }else{
+        cout << "Nao foi possivel remover item da lista";
+    }
+    cout << endl << endl;
+
+    //Com um valor que existe
+    aux.chave = 18;
+    cout << "Item no fim da lista: " << l.A[l.n_registros-1].chave << endl;
+    resp = l.remove(aux);
+    if (resp){
+        cout << "Item removido da lista!!";
+    }else{
+        cout << "Nao foi possivel remover item da lista";
+    }
+    cout << "Item no fim da lista: " << l.A[l.n_registros-1].chave << endl;
+    
     return 0;
 }
