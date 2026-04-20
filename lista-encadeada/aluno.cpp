19b2111a2bdf19310e3ccaac2fe94175006ea9af
JoaoPresley <jvalencarmg@gmail.com>
2026-04-20 20:10:17 -0300

    Iniciando criacao da lista encadeda

--- lista-encadeada/aluno.cpp
+++ lista-encadeada/aluno.cpp
@@ -2,12 +2,49 @@
 
 using namespace std;
 
-typedef struct {
+//Que será inserido na lista encadeada
+struct dado{
     //dados internos
     int id;
-    //dados de exposi
+    //dados de exposicao
+    char nome[30];
+    char rua[30];
+    char email[80];
+    dado *prox;
 
-}dado;
+};
+
+typedef dado no;
+
+typedef struct 
+{
+private:
+    //Lista interna (simplesmente encadeada)
+    struct lista{
+        int tam; //tamanho da minha lista encadeada
+        no *start;
+    };
+    //Cria a lista interna a lista interna
+    lista l;
+
+    //Constructor da lista encadeada
+    void chain_list(){
+        //apontado pro nada
+        l.start = NULL;
+        //tamanho 0
+        l.tam = 0;
+    }
+public:
+    //metodo para inserir no inicio
+    void insert_ini(dado d){
+        
+    };
+    //metodo para inserir no final
+    //metodo para mostrar a lista
+    //metodo para remover valor (lista duplamente encadeada)
+
+}chain_list;
+
 
 int main(){
     cout << "Hello wordl";
